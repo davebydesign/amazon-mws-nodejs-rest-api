c58f051b6bb35fded9598699b348d7a648618e50
davebydesign <davebydesign01@gmail.com>
2016-09-01 14:27:45 -0700

    Delete credentials.js

--- libs/mymws/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	AccessKeyId     : '0DAMF4S8ATPSYMJW1AG2',
-	SecretAccessKey : 'Gav3iKO4mgLJjWxye31ZyPe2detP6D0yd+2Pe1SD',
-	MerchantId      : 'A1SGWLLOJ9HNYJ',
-	MarketplaceId   : 'ATVPDKIKX0DER'
-};
